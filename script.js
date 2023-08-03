4bf329fd88e62f4ad70b923ee3724c77897d661c
Jan <janvilaaregut@gmail.com>
2023-08-03 18:06:53 +0200

    form

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector('.hamburger');
 const navList = document.querySelector('.nav__list');
+
 hamburger.addEventListener('click', () => {
   navList.classList.toggle('active');
   if (navList.classList.contains('active')) {
@@ -15,10 +16,15 @@ any.textContent = avui.getFullYear();
 
 function contact(event) {
   event.preventDefault();
-  emailjs.sendForm('service_bwtf09g', 'template_vypj8jv', event.target, 'eISCwA5cT5S9X_LWt');
-  console.log('Enviat').catch(() => {
-    alert(
-      'El servei de correu electrònic no està disponible temporalment. Si us plau, poseu-vos en contacte amb mi directament a lidia.molist@yahoo.es'
-    );
-  });
+  emailjs
+    .sendForm('service_bwtf09g', 'template_vypj8jv', event.target, 'eISCwA5cT5S9X_LWt')
+    .then(() => {
+      const form = document.querySelector('form');
+      form.reset();
+    })
+    .catch(() => {
+      alert(
+        'El servei de correu electrònic no està disponible temporalment. Si us plau, poseu-vos en contacte amb mi directament a lidia.molist@yahoo.es'
+      );
+    });
 }
